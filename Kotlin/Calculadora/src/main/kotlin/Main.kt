b99687c58b112a7240e1a28a53c987a443666392
patricia <patriciapiagn@gmail.com>
2022-05-19 16:53:54 -0300

    calculadora kotlin

---
+++ Kotlin/Calculadora/src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main(args: Array<String>) {
+
+
+    val x = 10
+    val y = 5
+
+    var resultado = soma(x, y)
+
+    println("O resultado é: $resultado ")
+
+}
+
+fun soma(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun multiplica(x: Int, y: Int) = x*y
+
+
+fun dividir(x: Int, y: Int): Int{
+    return x/y
+}
+
+fun subtrair(x: Int, y: Int) = x-y
+
+
+
+
+
+
+
+
+
